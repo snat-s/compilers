3a1e1812d039a7596ef9b27811d21942c0bfcfbb
snat-s <santpedrozadiaz@gmail.com>
2022-10-11 10:55:28 -0500

    Finished chapter Chunks of Bytecode

--- clox/debug.c
+++ clox/debug.c
@@ -2,13 +2,22 @@
 
 #include "chunk.h"
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name) {
-    printf("== %s ==", name);
+    printf("== %s ==\n", name);
 
     for (int offset = 0; offset < chunk->count;) {
         offset = disassembleInstruction(chunk, offset);
     }
+}
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset+1];
+    printf("%-16s %4d", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n'");
+    return offset + 2;
 }
 
 /* Esta parte en el libro no está bien documentada, le falta poner esta función en el debug.h*/
@@ -19,6 +28,12 @@ static int simpleInstruction(const char* name, int offset) {
 
 int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d", offset);
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset-1]) {
+        printf("   | ");
+    } else {
+        printf("%4d ", chunk->lines[offset]);
+    }
+
     uint8_t instruction = chunk->code[offset];
 
     switch (instruction) {
@@ -32,9 +47,4 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     }
 }
 
-static int constantInstruction(const char* name, Chunk* chunk, int offset) {
-    uint8_t constant = chunk->code[offset+1];
-    printf("%-16s %4d", name, constant);
-    printValue(chunk->constants.values[constant]);
-    printf("'\n'");
-}
+
--- clox/valu.h
+++
@@ -1,6 +0,0 @@
-#ifndef VALU_H_
-#define VALU_H_
-
-
-
-#endif // VALU_H_
